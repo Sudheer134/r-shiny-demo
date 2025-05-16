eac0ae42e89c74b551ec0ee449a28fbf6f0c7ef7
Sudheer Kumar Reddy Mayaluri <M324449@one.merckgroup.com>
2025-05-16 10:20:21 +0530

    created new file with eg with some more code

--- eg_prog.R
+++ eg_prog.R
@@ -8,6 +8,11 @@ library(tools)
 library(diffdf)
 
 
-prim <-  read_sas("root/M5717/G.20161801/0033/oversight/adam/1st/prod/outdata/adam_30_12_2024/adeg.sas7bdat")
-eg <- read_sas ( "root/M5717/G.20161801/0033/clinical_data/SDTM/stable/2024-11-14/eg.sas7bdat" )
-adsl <-read_sas("root/M5717/G.20161801/0033/oversight/adam/1st/dev/outdata/surv_prog_final_stats/adsl.sas7bdat")
+prim <-  read_sas("root/xxxx/G.20161801/0033/oversight/adam/1st/prod/outdata/adam_30_12_2024/adeg.sas7bdat")
+eg <- read_sas ( "root/xxxx/G.20161801/0033/clinical_data/SDTM/stable/2024-11-14/eg.sas7bdat" )
+adsl <-read_sas("root/xxxx/G.20161801/0033/oversight/adam/1st/dev/outdata/surv_prog_final_stats/adsl.sas7bdat")
+
+adsl1 <- adsl %>%
+  select(USUBJID, SAFFL, TRTSDTM, TRTSDT)
+eg1<- left_join(eg,adsl1, by="USUBJID",suffix = c("", "_remove")) %>%
+  filter(SAFFL == "Y" ) #& EGSTAT !="NOT DONE"
